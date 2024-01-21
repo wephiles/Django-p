039e5ef46f747493c7c4bc6521958c93dfca269f
wephiles <2086689759@qq.com>
2024-01-21 12:48:05 +0800

    添加所有文件

---
+++ django_web/printa_number.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @CreateTime : 2024/1/10 010 15:28
+# @Author : 瑾瑜@20866
+# @IDE : PyCharm
+# @File : studyProject/printa_number.py
+# @Description : 
+# @Interpreter : python 3.10
+# @Motto : You must take your place in the circle of life!
+# @Site : https://github.com/wephiles or https://gitee.com/wephiles
+
+def main():
+    for i in range(0, 101):
+        print(i, end=" ")
+    print()
+    return 0
+
+
+if __name__ == '__main__':
+    print(main())
+
+
+# END
